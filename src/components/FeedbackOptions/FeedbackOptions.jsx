ad0e6544d413f5614e13caa18a3cfc7e18e0eec1
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-07-26 23:49:50 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,21 +1,26 @@
-import { getColor } from "helpers/get-color"
-import { Button, List } from "./FeedbackOptions.styled"
-import PropTypes from "prop-types";
+import { getColor } from 'helpers/get-color';
+import { Button, List } from './FeedbackOptions.styled';
+import PropTypes from 'prop-types';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-
-    return (
-        <List>
-        {options.map(option => (
-            <li key={option.toLowerCase()}>
-                <Button name={option.toLowerCase()} color={getColor(option)} onClick={onLeaveFeedback}>{option}</Button></li>
-        ))}
-        </List>
-    )
-}
-
+  return (
+    <List>
+      {options.map(option => (
+        <li key={option.toLowerCase()}>
+          <Button
+            name={option.toLowerCase()}
+            color={getColor(option)}
+            onClick={onLeaveFeedback}
+          >
+            {option}
+          </Button>
+        </li>
+      ))}
+    </List>
+  );
+};
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
